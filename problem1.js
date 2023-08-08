d1cfec3b95c415afea0f6808514124b1c194b087
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-08-08 19:02:49 +0600

    problem 1 done

--- problem1.js
+++ problem1.js
@@ -1,14 +1,14 @@
 function cubeNumber(number) {
   const type1 = typeof number;
 
-  if (type1 === "string") {
-    return "Please enter number ";
+  if (type1 !== "number") {
+    return "Please enter number as a parameter ";
   }
 
   const result = Math.pow(number, 3);
   return result;
 }
 
-const res = cubeNumber(4);
+const res = cubeNumber(2);
 
 console.log(res);
