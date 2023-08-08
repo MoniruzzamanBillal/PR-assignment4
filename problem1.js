5e076e80ff7c7cc339e931a2370d29ef699f1345
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-08-08 18:47:51 +0600

    problem 1 done , without bonus

--- problem1.js
+++ problem1.js
@@ -1,1 +1,14 @@
-console.log("problem 1");
+function cubeNumber(number) {
+  const type1 = typeof number;
+
+  if (type1 === "string") {
+    return "Please enter number ";
+  }
+
+  const result = Math.pow(number, 3);
+  return result;
+}
+
+const res = cubeNumber(4);
+
+console.log(res);
