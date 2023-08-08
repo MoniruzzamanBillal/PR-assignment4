c9c149d4962b2ae1703e62c7bdeff8a2cc8c016f
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-08-08 22:11:06 +0600

    problem 2 done

---
+++ problem2.js
@@ -0,0 +1,35 @@
+function matchFinder(string1, string2) {
+  if (!string1 || !string2) {
+    return "Both input strings are required.";
+  }
+
+  let i, j, k;
+  let cou = 0;
+  let len1 = string1.length;
+  let len2 = string2.length;
+
+  for (i = 0; i < len1; i++) {
+    if (string2[0] === string1[i]) {
+      cou = 1;
+      for (j = 1, k = i + 1; j < len2; j++, k++) {
+        if (string2[j] === string1[k]) {
+          cou++;
+        }
+      }
+
+      if (cou === len2) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+console.log(matchFinder("John Doe", "ohn"));
+console.log(matchFinder("JavaScript", "Code"));
+console.log(matchFinder("Hello", ""));
+console.log(matchFinder("Hello", "World"));
+console.log(matchFinder("", "abc"));
+console.log(matchFinder("abc", ""));
+console.log(matchFinder("", ""));
